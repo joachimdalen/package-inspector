a4103c7b60bf14098aec7c59addcc7eb3c079348
Joachim Dalen <me@d3v.no>
2018-08-16 21:17:48 +0200

    Add test for scope parsing

--- test/test.js
+++ test/test.js
@@ -8,4 +8,11 @@ describe("packageInspector", async function() {
 		var res = await packageInspector("./");
 		expect(res.name.scoped).to.be.true;
 	});
+
+	it("should parse scope", async function() {
+		var res = await packageInspector("./");
+		expect(res.name.scope).to.be.string("joachimdalen");
+		expect(res.name.name).to.be.string("package-inspector");
+	});
+	
 });
