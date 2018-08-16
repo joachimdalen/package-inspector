f2a45ff58fe1b59c69e7247b477425d5dc548256
Joachim Dalen <me@d3v.no>
2018-08-16 21:04:43 +0200

    Add test for scoped name

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 "use strict";
 
 var expect = require("chai").expect;
-var packageInspector = require("./index").default;
+var packageInspector = require("../index");
 
 describe("packageInspector", async function() {
-
+	it("should be scoped", async function() {
+		var res = await packageInspector("./");
+		expect(res.name.scoped).to.be.true;
+	});
 });
